a1787621bcfa66624a7f03b8f1ce780e2fdeec90
student-vyt2.15 <student@sspu-opava.cz>
2017-09-18 08:44:50 +0200

    PVY

--- pvy3/01/js/main.js
+++ pvy3/01/js/main.js
@@ -14,4 +14,55 @@ function listOfAnimals(data, start, count){
     return (list.length > 0) ? list : '<p>No items</p>';
 }
 
-console.log(listOfAnimals(json,0,10))   ;
+function detailOfAnimal(data, id){
+    var animal = {};
+    try {
+        if(isNaN(id)) throw "Argument isn't a number";
+        if(id < 0) throw "Cannot be less than zero";
+        if(id > data.length-1) throw "Cannot be greater than animal list";
+        var i = 0;
+        while (i < data.length || !animal){
+            if(data[i].id == id) animal = data[i];
+            i++;
+
+        }
+    } catch(err) {
+        return '<div>Error: ' + err + '</div>';
+    }
+    return animal;
+}
+
+console.log(detailOfAnimal(json,5));
+
+
+
+
+
+
+
+$(function(){
+    var animals = [];
+    var animal = {};
+    function listView(start, count){
+        animals = listOfAnimals(json,start,count);
+        animal = animals[0];
+        $('#list').html('<div class="list-group"></div>');
+        animals.forEach(function(item, index){
+            $('#list .list-group').append('<a href="#" data-id="'+ item.id +'" class="list-group-item">'+item.title+'</a>');
+        });
+        itemClick();
+    }
+
+    function detailView(animal) {
+        $('#detail').html('<h3>' + animal.title + '</h3>');
+    }
+
+    function itemClick(){
+        $('#list .list-group-item').on('click', function(){
+            animal = detailOfAnimal(json,$(this).data('id'));
+            detailView(animal);
+        })
+    }
+    listView(0,10);
+    detailView(animal);
+})
