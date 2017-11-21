7e3e00607b060a237e2a3d67d4959b0745f4fcd2
ruduvun <kavka.radovan@gmail.com>
2017-11-21 22:53:50 +0100

    Window-only build

---
+++ pcv/Projekt/src/projekt/Click.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projekt;
+
+/**
+ *
+ * @author Radovan
+ */
+public class Click extends Window {
+    private int clicks = 0;
+    private int clickIncome = 0;
+    
+    public Click (int clicks, int clickIncome){
+        this.clicks = clicks;
+        this.clickIncome = clickIncome;
+    }
+    
+    private void onClick(){
+        
+    }
+}
